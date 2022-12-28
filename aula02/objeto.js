cb146ed5a3e200fee0688ca63ece362712d09691
Henrique Yamin <121504273+henriqueyamin@users.noreply.github.com>
2022-12-28 11:55:56 -0300

    Add files via upload

---
+++ aula02/objeto.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "João",
+    dataNascimento: "27/12/2007",
+    CPF: "03514233213",
+    Email: "dksjfhsk@gmail.cliente",
+    Cidade: 'Brasilia',
+    Estado: 'DF'
+    }
+    
+
+    /* quero mudar o nome do cliente */
+    delete cliente.Estado
+    console.log (cliente) 
+    
---
+++ aula02/script.js
@@ -0,0 +1,26 @@
+/*const testeAULA = 3 // constante nao muda
+console.log(testeAULA)
+
+let var1 = 4; //number
+let var2 = 5; //number
+let var3 = "aula"; //string
+let var4 = true; //instance
+if (var3 === number) {
+    return true;
+} else if (var3 === string) {
+    return console.log("Não é um número")
+
+}*/
+
+
+let forma = "RETANGULO";
+let altura = 5;
+let comprimento = 7;
+let area;
+if (forma === "RETANGULO") {
+    area = altura * comprimento;
+}
+else {
+    area = (altura * comprimento)/2;
+}
+console.log(area);
